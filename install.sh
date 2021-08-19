235f0fa1a9a48e935d4613f60cb0a3211432600f
wgdp444 <whigdrap@gmail.com>
2021-08-20 03:04:32 +0900

    update: neovimをインストール

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@
 ./install_fish.sh
 
 
+# Install Neovim
+if [ "$(uname)" == 'Darwin' ]; then
+    brew install neovim
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    sudo apt install neovim || apt install neovim
+fi
+
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
