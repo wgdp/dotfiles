b9215eddc08caf8b304e145a18e2e88e82076ccd
wgdp444 <tatsuyori.shiraga@triple-four.com>
2021-11-19 18:26:21 +0900

    update: init.sh

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/b
 
 # install git
 sudo apt update | apt update
-sudo apt install git | apt install git
+sudo apt install -y git | apt install -y git
 
 # clone dotfiles
 git clone https://github.com/wgdp444/dotfiles
