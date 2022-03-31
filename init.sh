934be4897458474cc3d3b8f5e2c8fd86092f106b
wgdp <whigdrap@gmail.com>
2022-04-01 02:03:13 +0900

    update: init.sh Gitのインストール処理を削除

--- init.sh
+++ init.sh
@@ -2,21 +2,5 @@
 
 set -eux
 
-DOTFILES_URL = https://github.com/wgdp444/dotfiles
-
-if [ "$(uname)" == 'Darwin' ]; then
-    brew update
-    brew install git
-
-    git clone DOTFILES_URL
-    cd dotfiles/macos
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    sudo apt update
-    sudo apt install
-
-    git clone DOTFILES_URL
-    cd dotfiles/debian
-fi
-
 ../link.sh
 ./install.sh
