8c856a1a437c007b63aa487fa25e51f695853b4a
wgdp <tatsuyori4649@gmail.com>
2022-07-01 16:57:04 +0900

    🌿 Neovim初期化時に実行するやつ

--- neovim.sh
+++ neovim.sh
@@ -1,3 +1,6 @@
 # !/bin/bash
 
 nvim +PackerInstall +PackerCompile +qall
+nvim +LspInstall gopls efm marksman omnisharp pyright rust_analyzer sumneko_lua +qall
+nvim +TSInstall c go rust lua ruby python c_sharp bash markdown markdown_inline cpp fish javascript +qall
+
