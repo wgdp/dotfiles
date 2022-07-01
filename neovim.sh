ed6479e41158fd2627b9a0e5f3adddcb5e4d4415
wgdp <whigdrap@gmail.com>
2022-07-01 17:16:35 +0900

    🌿 TSInstallに追加

--- neovim.sh
+++ neovim.sh
@@ -2,5 +2,27 @@
 
 nvim +PackerInstall +PackerCompile +qall
 nvim +LspInstall gopls efm marksman omnisharp pyright rust_analyzer sumneko_lua +qall
-nvim +TSInstall c go rust lua ruby python c_sharp bash markdown markdown_inline cpp fish javascript +qall
+nvim +TSInstall \
+    c \
+    go \
+    rust \
+    lua \
+    ruby \
+    python \
+    c_sharp \
+    bash \
+    markdown \
+    markdown_inline \
+    cpp \
+    fish \
+    javascript \
+    json \
+    json5 \
+    toml \
+    yaml \
+    html \
+    css \
+    scss \
+    java \
++qall
 
