bca61b3de5a2efb747db88d10171068ad41ada33
wgdp <tatsuyori4649@gmail.com>
2022-09-13 13:59:42 +0900

    🌿 telescope nvimの設定を追加

--- .config/nvim/lua/modules/telescope.lua
+++ .config/nvim/lua/modules/telescope.lua
@@ -1,3 +1,10 @@
+require('telescope').setup{
+    file_ignore_patterns = {
+        "^node_modules/",
+        "^vender/"
+    }
+
+}
 vim.api.nvim_set_keymap('n', '<space>f', '<Cmd>Telescope find_files<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<space>r', '<Cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<space>h', '<Cmd>Telescope buffers<CR>', { noremap = true, silent = true })
