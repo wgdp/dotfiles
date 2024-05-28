be9ea3fcd0ec02e9656659e86b29ff81f2837eed
wgdp <55125267+wgdp@users.noreply.github.com>
2024-05-28 15:13:38 +0900

    Update apt.sh

--- debian/apt.sh
+++ debian/apt.sh
@@ -1,26 +1,10 @@
 # apt周り
 
 # リポジトリ追加
-sudo add-apt-repository ppa:lazygit-team/release
 sudo add-apt-repository ppa:longsleep/golang-backports
 
 sudo apt update -y
 
 sudo apt install -y \
-    lazygit \
     wget \
     golang
-
-# neovim用
-sudo apt install -y \
-    ninja-build \
-    gettext \
-    libtool \
-    libtool-bin \
-    autoconf \
-    automake \cmake \
-    g++ \
-    pkg-config \
-    unzip \
-    curl \
-    doxygen
