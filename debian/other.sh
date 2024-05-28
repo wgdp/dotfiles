3bd8f44f4fa86520609d32708a52d80d6dd11c2a
wgdp <55125267+wgdp@users.noreply.github.com>
2024-05-28 15:13:06 +0900

    Update other.sh

--- debian/other.sh
+++ debian/other.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
 
 # neovim
-NEOVIM_VERSION="v0.7.2"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-cd /tmp
-git clone https://github.com/neovim/neovim
-cd /tmp/neovim
-git checkout $NEOVIM_VERSION
-
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
-
-sudo mv ./build/bin/nvim /user/bin/nvim
-
-# pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
