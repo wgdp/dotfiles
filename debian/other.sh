2626382fe07b4c780425e97f868b91a89eeced46
wgdp <55125267+wgdp@users.noreply.github.com>
2024-05-28 15:31:30 +0900

    Update other.sh

--- debian/other.sh
+++ debian/other.sh
@@ -4,9 +4,18 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo rm -rf nvim-linux64.tar.gz
 
 # lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+tar xf /tmp/lazygit.tar.gz /tmp/lazygit
+sudo install /tmp/lazygit /usr/local/bin
+sudo rm -rf lazygit
+sudo rm -rf lazygit.tar.gz
+
+# golang
+GO_VERSION=1.22.3
+wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+sudo rm -rf go${GO_VERSION}.linux-amd64.tar.gz
